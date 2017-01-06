89d68f490fa03468eba92a55f95435454b04266f
Tyler Long <tyler4long@gmail.com>
2017-01-06 15:28:10 +0800

    JS standard style

--- src/id_utilities.js
+++ src/id_utilities.js
@@ -1,37 +1,37 @@
-// Provides utilities for handling IDs 
-var ID_Utilities = function() {
-};
+// Provides utilities for handling IDs
+var idUtilities = function () {
+}
 
 // Define mask for accessing the object type field
-ID_Utilities.prototype.TYPE_MASK = 0x1fff;
+idUtilities.prototype.TYPE_MASK = 0x1fff
 
-// 
+//
 // Public
 //
 // ID_Utilities.extract_type();
 //
 // purpose: Extract the type from a given ID
-// arguments: 
+// arguments:
 //    id: id from which to extract type
 // returns: the type as an integer
 //
-ID_Utilities.prototype.extract_type = function(id) {
-	return id & this.TYPE_MASK;
-};
+idUtilities.prototype.extract_type = function (id) {
+  return id & this.TYPE_MASK
+}
 
-// 
+//
 // Public
 //
 // ID_Utilities.extract_pure_id();
 //
 // purpose: Extract the pure ID (without type) from a given ID
-// arguments: 
+// arguments:
 //    id: id from which to extract pure id
 // WARNING: THIS IS A BIT CPU INTENSIVE!!!
 // returns: the pure ID
 //
-ID_Utilities.prototype.extract_pure_id = function(id) {
-	return Math.floor(id/(this.TYPE_MASK+1));	
-};
+idUtilities.prototype.extract_pure_id = function (id) {
+  return Math.floor(id / (this.TYPE_MASK + 1))
+}
 
-module.exports = ID_Utilities;
+module.exports = idUtilities
