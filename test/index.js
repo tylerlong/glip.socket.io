76ed6dedacb7600daf547a9ef5eac326ee1876df
Tyler Long <tyler4long@gmail.com>
2018-03-16 15:26:55 +0800

    First working testing code

--- test/index.js
+++ test/index.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const GlipSocket = require('../src/index.bundle')
+
+const client = new GlipSocket({
+  host: process.env.GLIP_HOST || 'app.glip.com',
+  port: process.env.GLIP_PORT || 443,
+  user: process.env.GLIP_EMAIL,
+  password: process.env.GLIP_PASSWORD
+})
+
+client.on('message', (type, data) => {
+  console.log(type, data)
+})
+
+client.start()
