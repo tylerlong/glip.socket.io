dc6769219d3fc0611b23e3fb8752e1a1f864d483
Tyler Long <tyler4long@gmail.com>
2019-09-12 09:58:19 -0700

    Update upload_from_url.js

--- test/upload_from_url.js
+++ test/upload_from_url.js
@@ -5,5 +5,5 @@ This doesn't work any more. Because endpoint `/api/file-from-url` has been close
 const client = require('./index')
 
 client.on('started', () => {
-  client.post_file_from_url(16130957314,'https://glip-vault-1.s3.amazonaws.com/web/customer_files/530850668556/modified.png?Expires=2075494478&AWSAccessKeyId=AKIAJROPQDFTIHBTLJJQ&Signature=AeykXwQ63Tz3SkuJS7Dn9hsbf7Q%3D', 'Hey, look at this cool icon!')
+  client.post_file_from_url(16130957314,'http://example.com/image.png', 'Hey, look at this cool icon!')
 })
